9261001b0cf806c9c626e63b09701ade852b454e
PermutationSpace <alban.dutilleul@ens-rennes.fr>
2021-10-14 16:55:25 +0200

    Logics & Basic operations

--- scripts/arith.sh
+++ scripts/arith.sh
@@ -1,5 +1,5 @@
 tigress --Environment=x86_64:Linux:Gcc:9.3 \
         --Transform=EncodeArithmetic\
-        --Functions=equal,not_equal,main \
+        --Functions=equal,not_equal,superior,inferior,main,add,sub,mul,bdiv \
         --out=./../res/generated/arith.c \
         --Seed=0 ./../src/templates/arith.c
